912190b93733881432f7763e81bc694deef2e9c3
Abhik Dutta <abhikprogrammer@gmail.com>
2026-05-29 19:22:08 +0530

    Editted BMI_calculator.py

--- day2/BMI_calculator.py
+++ day2/BMI_calculator.py
@@ -2,3 +2,9 @@ x = float(input("Give weight in kilograms:   "))
 y = float(input("Give height in metres:     "))
 
 print("Your BMI is:  ", str(x/(y*y)))
+
+if (x/(y*y))>18.5 and (x/(y*y))<24.9:
+    print("You are healthy")
+
+else:
+    print("You are not healthy")
