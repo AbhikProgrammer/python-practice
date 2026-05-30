93e17a6858d1b82064c55d47d3898b2801bb7e6e
Abhik Dutta <abhikprogrammer@gmail.com>
2026-05-30 10:36:43 +0530

    Add scoring system

--- Week_1/day4/two_dig_add.py
+++ Week_1/day4/two_dig_add.py
@@ -4,14 +4,19 @@ i = int(input("How many Multiplications do u wanna practise?   "))
 x = random.randint(10, 99)
 y = random.randint(10, 99)
 
+score = 0
+
 for r in range (i):
     x = random.randint(10, 99)
     y = random.randint(10, 99)
 
-    while True:
-        prod = int(input("What is "+str(x)+"x"+str(y)+"?"))
-        if prod == x*y:
-            print("Correct")
-            break
-        else:
-            print("Incorrect")
+
+    prod = int(input("What is "+str(x)+"x"+str(y)+"?"))
+    if prod == x*y:
+        print("Correct")
+        score = score + 1
+        
+    else:
+        print("Incorrect")
+
+print("You achieved, "+str(score)+"/"+str(i))
