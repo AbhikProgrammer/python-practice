dd1ba85ed960f9efa1ce4197a630ceb6838d474b
Abhik Dutta <abhikprogrammer@gmail.com>
2026-06-03 08:13:20 +0530

    Modified math_functions.py

---
+++ Week_1/day5/math_functions.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+def diff(a, b):
+    return a - b
+
+a = int(input("Give first number:   "))
+b = int(input("Give second number:  "))
+
+print("Sum of the numbers is, ", str(add(a, b)))
+print("Difference of the numbers is, ", str(diff(a, b)))
